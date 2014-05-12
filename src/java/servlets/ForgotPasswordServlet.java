98b834c16e9e9b270c65855f1029390d58c40c49
alessandroTorresani <alessandro@siel.it>
2014-05-12 19:46:24 +0200

    fix build problem

--- src/java/servlets/ForgotPasswordServlet.java
+++ src/java/servlets/ForgotPasswordServlet.java
@@ -20,8 +20,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
-import static servlets.ControlRequestServlet.log;
-import static servlets.RegistrationServlet.log;
 import utils.Mailer;
 
 /**
