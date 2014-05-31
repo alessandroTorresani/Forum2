51129a26dbf5532664d7c87726ccf7a35945f287
marcolyon <m.lion920@gmail.com>
2014-05-31 14:51:56 +0200

    character accented groupName

--- src/java/servlets/CreateGroupServlet.java
+++ src/java/servlets/CreateGroupServlet.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package servlets;
 
 import db.DBManager;
@@ -27,11 +26,11 @@ import org.apache.log4j.Logger;
  * @author Alessandro
  */
 public class CreateGroupServlet extends HttpServlet {
-    
+
     private DBManager manager;
-    private String GROUPNAME_REGEX = "^[[a-zA-Z0-9_-]+(\\s[a-zA-Z0-9_-]+)*]{3,20}$";
+    private String GROUPNAME_REGEX = "^[[a-zA-Z0-9àáâäèéêëìíîïòóôöùúûü_-]+(\\s[a-zA-Z0-9àáâäèéêëìíîïòóôöùúûü_-]+)*]{3,20}$";
     static Logger log = Logger.getLogger(RegistrationServlet.class.getName());
-    
+
     public void init() throws ServletException {
         this.manager = (DBManager) super.getServletContext().getAttribute("dbmanager");
     }
@@ -47,47 +46,68 @@ public class CreateGroupServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         String groupName = request.getParameter("groupName");
         boolean is_private = "private".equals(request.getParameter("optionsRadios"));
-        
+
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date date = new Date();
-        
+
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
         int groupId = 0;
-        System.out.println(is_private);
-        
-        
-        if ((groupName != null) && (groupName.matches(GROUPNAME_REGEX))){
+
+        //accenti
+        System.out.println(groupName);
+
+        groupName = groupName.replace("Ã ", "à");
+        groupName = groupName.replace("Ã¡", "á");
+        groupName = groupName.replace("Ã¢", "â");
+        groupName = groupName.replace("Ã¤", "ä");
+        groupName = groupName.replace("Ã¨", "è");
+        groupName = groupName.replace("Ã©", "é");
+        groupName = groupName.replace("Ãª", "ê");
+        groupName = groupName.replace("Ã«", "ë");
+        groupName = groupName.replace("Ã¬", "ì");
+        groupName = groupName.replace("Ã­", "í");
+        groupName = groupName.replace("Ã®", "î");
+        groupName = groupName.replace("Ã¯", "ï");
+        groupName = groupName.replace("Ã²", "ò");
+        groupName = groupName.replace("Ã³", "ó");
+        groupName = groupName.replace("Ã´", "ô");
+        groupName = groupName.replace("Ã¶", "ö");
+        groupName = groupName.replace("Ã¹", "ù");
+        groupName = groupName.replace("Ãº", "ú");
+        groupName = groupName.replace("Ã»", "û");
+        groupName = groupName.replace("Ã¼", "ü");
+
+        System.out.println(groupName);
+
+        if ((groupName != null) && (groupName.matches(GROUPNAME_REGEX))) {
             try {
-            groupId = manager.createGroup(user.getUserId(), groupName, dateFormat.format(date), is_private);
-            } catch (SQLException ex){
+                groupId = manager.createGroup(user.getUserId(), groupName, dateFormat.format(date), is_private);
+            } catch (SQLException ex) {
                 log.error(ex.toString());
                 throw new ServletException(ex);
             }
-            if (groupId > 0){
+            if (groupId > 0) {
                 try {
-                manager.subscribeAdmin(groupId, user.getUserId());
-                log.info("User " + user.getEmail() + " create a new group with id: " + groupId);
-                } catch (SQLException ex){
+                    manager.subscribeAdmin(groupId, user.getUserId());
+                    log.info("User " + user.getEmail() + " create a new group with id: " + groupId);
+                } catch (SQLException ex) {
                     log.error(ex.toString());
                     throw new ServletException(ex);
                 }
-                 response.sendRedirect(request.getContextPath() + "/GetOwnerGroups?email=" + user.getEmail());
+                response.sendRedirect(request.getContextPath() + "/GetOwnerGroups?email=" + user.getEmail());
             } else {
                 System.out.println("no group_id");
                 response.sendRedirect(request.getContextPath() + "/createGroup.jsp");
             }
         } else {
-             System.out.println("no correct name");
+            System.out.println("no correct name");
             response.sendRedirect(request.getContextPath() + "/createGroup.jsp");
         }
-        
-        
-        
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
